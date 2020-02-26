93b60633ec70c7309eeaa699f00fd4b6d9b0c07f
aidan-kelly <it.is.aidan@gmail.com>
2020-02-26 16:06:23 -0700

    Colours and nicknames work. No error checking currently.

--- client.js
+++ client.js
@@ -14,20 +14,35 @@ $(function () {
     //when client gets a message
     socket.on("chat message", function(msg){
         //add the message to the list of messages
+        console.log(msg);
         msg = JSON.parse(msg);
-        formatted_msg = msg.timestamp + " " + msg.user + " " + msg.msg;
+        formatted = msg.timestamp + ` <span class='${msg.id}'>` + msg.user + "</span>: " + msg.msg;
 
         let newListItem = $("<li>");
-        newListItem.text(formatted_msg);
-
+        newListItem.html(formatted);
         if(msg.user == username){
-            newListItem.css("font-weight","Bold");
+            newListItem.css({"font-weight":"Bold"});
             $('#messages').append(newListItem);
+            $(`.${msg.id}`).css({"color":`#${msg.colour}`});
         }else{
             $('#messages').append(newListItem);
+            $(`.${msg.id}`).css({"color":`#${msg.colour}`});
         }
         
     });
+
+    socket.on("username update", function(msg){
+        $('#messages').append($("<li>").text(msg));
+        if(msg.split(" ")[0] == username){
+            username = msg.split(" ")[6];
+        }
+        
+    });
+
+    socket.on("colour update", function(msg, userID, colour){
+        $('#messages').append($("<li>").text(msg));
+        $(`.${userID}`).css({"color":`#${colour}`});
+    })
 
     //when a user first joins the chat
     //they are given a username
--- server.js
+++ server.js
@@ -5,6 +5,9 @@ app.use(express.static("."));
 var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
+//our dictionary of all users
+let user_list = new Object();
+
 //when a user loads our website, they are sent the index.html file.
 app.get("/", function(req, res){
     res.sendFile(__dirname + "/index.html");
@@ -13,23 +16,30 @@ app.get("/", function(req, res){
 //handles connections from a socket
 io.on("connection", function(socket){
 
-    //maintain a username for each client. Might need to make this persistant and not in memory
-    let username = getUsername(socket);
+    //make a new default user
+    let new_user = new User(socket.id);
+
+    //create a new dictionary entry with the socket id as the key
+    user_list[new_user.userID] = new_user;
 
     //when a client first connects we send them their username
-    socket.emit("username message", username);
+    socket.emit("username message", user_list[socket.id].userNickname);
+    console.log(`A user connected. ID = ${user_list[socket.id].userNickname}.`);
 
-    console.log(`A user connected. ID = ${username}.`);
     //when a socket disconnects
     socket.on('disconnect', function(){
         console.log('user disconnected');
-      });
+        user_list[new_user.userID].status = false;
+    });
 
     //when we receive a message from a client
     socket.on("chat message", function(msg){
-        //send that message to all clients
-        msg = new Message(msg, getUsername(socket))
-        io.emit("chat message", JSON.stringify(msg));
+
+        msg = new Message(msg, user_list[socket.id].userNickname, user_list[socket.id].userColour, socket.id);
+
+        //check if user is trying to issue a command
+        checkForCommand(msg, socket.id);
+        
     });
 });
 
@@ -37,6 +47,7 @@ io.on("connection", function(socket){
 http.listen(3000, function(){
     console.log("listening on port 3000");
 })
+
 
 //returns a formatted timestamp
 function getTimeStamp(){
@@ -50,14 +61,65 @@ function getTimeStamp(){
     return ("[" + time[0] + ":" + time[1] + ":" + time[2] + "]");
 }
 
-//might need to change this, want to make username persistant somehow
-function getUsername(socket){
-    return socket.id;
-}
 
 //constructor for a Message object
-function Message(msg, user){
+function Message(msg, user, colour, id){
     this.msg = msg;
     this.user = user;
+    this.id = id;
     this.timestamp = getTimeStamp();
+    this.colour = colour;
+}
+
+
+//constructor for a User object
+function User(userID){
+    this.userID = userID;
+    this.userNickname = userID;
+    this.userColour = "000000";
+    this.status = true;
+}
+
+
+function checkForCommand(msg, userID){
+    let split_msg = msg.msg.split(" ");
+
+    if(split_msg[0] === "/nick"){
+        changeNickname(split_msg, userID);
+        return 1;
+    }else if(split_msg[0] === "/nickcolor"){
+        changeColour(split_msg, userID);
+        return 2;
+    }else if(split_msg[0] === "/nickcolour"){
+        changeColour(split_msg, userID);
+        return 2;
+    }else{
+        io.emit("chat message", JSON.stringify(msg));
+    } 
+}
+
+
+function changeNickname(command, userID){
+
+    //still need to implement check for existing usernames
+    if(command[1] !== undefined){
+        old_nickname = user_list[userID].userNickname;
+        user_list[userID].userNickname = command[1];
+        io.emit("username update", `${old_nickname} has changed their username to ${user_list[userID].userNickname}`);
+        
+    }else{
+        console.log("FUCK");
+    }
+}
+
+
+function changeColour(command, userID){
+    //still need to implement check for existing usernames
+    if(command[1] !== undefined){
+        user_list[userID].userColour = command[1];
+        io.emit("colour update", `${user_list[userID].userNickname} changed their colour!`, userID, user_list[userID].userColour);
+        
+    }else{
+        console.log("FUCK");
+    }
 }
